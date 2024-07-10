6f05b6513bb2a96e273ac2f68fe362868af33d42
khansrfrz <khansarfaraz205@gmail.com>
2024-07-10 13:48:31 +0530

    image added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Header from "./common/Header";
+import Footer from "./common/Footer";
+import {Route,BrowserRouter as Router, Routes} from "react-router-dom"
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import NotFoundPage from "./pages/NotFoundPage";
+import Shop from "./pages/Shop";
+import ContactUs from "./pages/Contact";
+import Blog from "./pages/Blog";
 
 const App = () => {
   return (
     <div>
-      <h1 className=' text-3xl font-bold underline'>Hello</h1>
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route path='/' element={<Home></Home>} />
+          <Route path='/cart' element={<Cart></Cart>} />
+          <Route path='/shop' element={<Shop></Shop>} />
+          <Route path='/contact' element={<ContactUs></ContactUs>} />
+          <Route path='/about' element={<AboutUs></AboutUs>} />
+          <Route path='/blog' element={<Blog></Blog>} />
+          <Route path='/*' element={<NotFoundPage></NotFoundPage>} />
+        </Routes>
+        <Footer></Footer>
+      </Router>
     </div>
   )
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {banners} from "../data/Data";
+
+
+const Banner = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Banner
